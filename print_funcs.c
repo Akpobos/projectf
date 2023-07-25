375bf7d4110fad7a016103849acd122b7355922e
Emmanuel Alabi <alabi.akpobome.emmanuel@gmail.com>
2023-07-25 01:34:43 -0700

    gg

--- print_funcs.c
+++ print_funcs.c
@@ -40,7 +40,7 @@ int print_str(va_list args)
  */
 int print_int(va_list args)
 {
-	int val = va_arg(args, int), count = 0;
+	long int val = va_arg(args, int), count = 0;
 
 	if (val < 0)
 		count += _putchar('-');
